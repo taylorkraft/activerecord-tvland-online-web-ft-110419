cf5d7fa4010d9f1f523c812ca1cd9a7432134655
Taylor Kraft <taylorkraft16@gmail.com>
2020-01-03 21:24:49 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,7 +1,8 @@
 class CreateCharacters < ActiveRecord::Migration[5.1]
   def change
     create_table :characters do |t|
-      t.string :name
+      t.string :first_name
+      t.string :last_name
       t.integer :actor_id
       t.integer :show_id
     end
