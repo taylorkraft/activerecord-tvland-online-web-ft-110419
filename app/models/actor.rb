bce6916db4cbf17542b14c4e6466085507ce7f11
Taylor Kraft <taylorkraft16@gmail.com>
2020-01-03 21:34:30 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,6 +7,8 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    self.characters.shows
+    self.characters.collect do |c|
+      "#{c.name} - #{c.show.name}"
+    end.join
   end
 end
