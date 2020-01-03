ecab12d8800085eb9ea517be8cdd91c321832b4a
Taylor Kraft <taylorkraft16@gmail.com>
2020-01-03 21:28:31 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 9) do
+ActiveRecord::Schema.define(version: 8) do
 
   create_table "networks", force: :cascade do |t|
     t.string "call_letters"
