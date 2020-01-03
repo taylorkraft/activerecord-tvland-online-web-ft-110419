2435e85f23105ced1758f1a60949e304b563cad4
Taylor Kraft <taylorkraft16@gmail.com>
2020-01-03 20:49:13 +0000

    Automatically backed up by Learn

--- db/migrate/006_add_catch_phrase_to_character.rb
+++ db/migrate/006_add_catch_phrase_to_character.rb
@@ -1,4 +1,4 @@
-class AddCatchPhraseToCharacters < ActiveRecord::Migration[5.1]
+class AddCatchPhraseToCharacter < ActiveRecord::Migration[5.1]
   def change
     add_column :characters, :catchphrase, :string
   end
